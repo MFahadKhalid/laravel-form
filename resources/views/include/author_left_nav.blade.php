d785cd1f59d53be8badd1f25f408f5c9501bdcdd
abdulwahabkhalid <98513650+MFahadKhalid@users.noreply.github.com>
2022-10-11 11:34:35 +1400

    laravel-auth

--- resources/views/include/author_left_nav.blade.php
+++ resources/views/include/author_left_nav.blade.php
@@ -10,7 +10,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-        <img src="{{asset('upload/thumbnail/'.$user->thumbnail)}}" alt="" class="" height="100%" width="100%">
+        <img style="height:100%; " src="{{asset('upload/thumbnail/'.$user->thumbnail)}}" alt="" class="img-rounded" height="100%" width="100%">
       </div>
         <div class="info">
           <a href="{{ route('profile.edit')}}" class="d-block">{{auth()->user()->name}}</a>
