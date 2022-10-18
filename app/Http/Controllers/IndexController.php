4bc708235db8465ab0caa1e7b33529234e7a92ab
Qasim Inayat <qasiminayat93@gmail.com>
2022-10-18 23:05:32 +0500

    18-10-2022

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -19,7 +19,16 @@ class IndexController extends Controller
         $blog = Blog::where('id',$id)->firstOrFail();
         $alsoLike = Blog::where('category_id', $blog->category_id)->take(3)->orderBy('id','DESC')->get();
         $categories  = Category::where('status',1)->orderBy('name','ASC')->get();
-        return view('web.pages.details' , compact('blog','alsoLike','categories'));
+        $popularPosts = Blog::orderBy('views','DESC')->take(4)->get();
+
+        //Views Count
+        $oldViews = $blog->views;
+        $newViews = $oldViews + 1;
+
+        //Updating Views to Blog table
+        $views  = Blog::where('id',$id)->update(['views' => $newViews]);
+
+        return view('web.pages.details' , compact('blog','alsoLike','categories','popularPosts'));
     }
     public function categoryWise($id){
         $blog = Category::where('id',$id)->firstOrFail();
